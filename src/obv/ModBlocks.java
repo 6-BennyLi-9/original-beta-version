a1540bb348cb5b4b39132230ed07323fa4e2f972
Benny0Li <bennyli2011@163.com>
2024-05-02 21:40:30 +0800

    Optimized building building time(haha)

--- src/obv/ModBlocks.java
+++ src/obv/ModBlocks.java
@@ -1,11 +1,12 @@
 package obv;
+import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.blocks.defense.Wall;
 import mindustry.world.meta.BuildVisibility;
 
 public class ModBlocks implements ContentList{
-    
+
     public static Wall bigIronWall;
     public static Wall smallIronWall;
 
@@ -22,13 +23,15 @@ public class ModBlocks implements ContentList{
 
         bigIronWall.requirements(Category.defense,BuildVisibility.shown,new ItemStack[]{
             new ItemStack(ModItems.iron,5),
-            new ItemStack(ModItems.glass,3)
+            new ItemStack(ModItems.glass,3),
+            new ItemStack(Items.metaglass,3)
         });
         smallIronWall.requirements(Category.defense,BuildVisibility.shown,new ItemStack[]{
             new ItemStack(ModItems.iron,20),
-            new ItemStack(ModItems.glass,12)
+            new ItemStack(ModItems.glass,12),
+            new ItemStack(Items.metaglass,7)
         });
-            bigIronWall.buildCostMultiplier=1.25f;
+        bigIronWall.buildCostMultiplier=1.25f;
         smallIronWall.buildCostMultiplier=0.8f;
     }
     public void load(){
--- src/obv/ModItems.java
+++ src/obv/ModItems.java
@@ -8,6 +8,7 @@ public class ModItems implements ContentList{
     public static Item ionicPhaseFabric;
     public static Item fullMobileBattery;
     public static Item emptyMobileBattery;
+    private static float unitCost=20f;
 
     private void initItem(){
         iron=new Item("iron");
@@ -26,6 +27,11 @@ public class ModItems implements ContentList{
 
         //放射性，与RTG直接相关
         ionicPhaseFabric.radioactivity=5;
+
+        //花费，实际上指的是每单位物品所带来的制造时间
+        iron.cost=8*unitCost;
+        glass.cost=16*unitCost;
+        ionicPhaseFabric.cost=23*unitCost;
     }
     private void setupItemColorConfig() {
         iron.color=utils.RGBColorSetWitchHTMLMark(0xaeb7ff);
