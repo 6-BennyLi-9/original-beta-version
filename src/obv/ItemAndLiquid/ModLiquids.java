74683942f7dedf12e64324d03dab854068764894
Benny0Li <bennyli2011@163.com>
2024-06-08 16:46:44 +0800

    Upload Liquids' name and description

--- src/obv/ItemAndLiquid/ModLiquids.java
+++ src/obv/ItemAndLiquid/ModLiquids.java
@@ -1,10 +1,10 @@
 package obv.ItemAndLiquid;
 
-import arc.util.Log;
-import mindustry.type.Liquid;
-import obv.ModContent;
+import arc.util.*;
+import mindustry.type.*;
+import obv.*;
 
-public class ModLiquids implements ModContent {
+public class ModLiquids implements ModContent{
 
     public static Liquid essentialOil;
     public static Liquid tila;
@@ -13,15 +13,15 @@ public class ModLiquids implements ModContent {
     public static Liquid superMagicalLiquid;
 
     public void initLiquid(){
-        essentialOil=new Liquid("essentialOil");
-        tila=new Liquid("tila");
-        tila_Beta=new Liquid("Beta-tila");
-        HighPurityEssentialOil=new Liquid("HighPurityEssentialOil");
-        superMagicalLiquid=new Liquid("superMagicalLiquid");
+        essentialOil = new Liquid("bioEssentialOil");
+        tila = new Liquid("tila");
+        tila_Beta = new Liquid("Beta-tila");
+        HighPurityEssentialOil = new Liquid("HighPurityBioEssentialOil");
+        superMagicalLiquid = new Liquid("superMagicalLiquid");
     }
 
     @Override
-    public void load() {
+    public void load(){
         Log.info("[UBV-ITEMS]Start init");
         initLiquid();
     }
